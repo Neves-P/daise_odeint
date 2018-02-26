417a887fee31f1fb68a2dce7f26692dc7d765f4d
Neves-P <10762799+Neves-P@users.noreply.github.com>
2018-02-26 18:46:10 +0100

    EOD

--- run code.R
+++ run code.R
@@ -2,11 +2,11 @@
 
 source("make_rhs_string.R")
 
-ddep_vec <- c(0, 11, 21)
+ddep_vec <- c(0, 1, 12)
 time_to_run <- c()
-for (i in 1){
+for (i in 3){
   t_start <- Sys.time()
-  test_integrators(300, ddep = ddep_vec[i])
+  test_integrators(300, ddep = ddep_vec[i], kk = 1, K = 40)
   t_end <- Sys.time()
   time_to_run[i] <- t_end - t_start
 }
