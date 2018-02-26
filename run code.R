b226c568b442da68aa9ade2a15d09e3c210c5c0d
P.M. Santos neves <P282067@workspace.rug.nl>
2018-02-26 15:02:05 +0100

    Update run code for short run

--- run code.R
+++ run code.R
@@ -6,7 +6,7 @@ ddep_vec <- c(2, 11, 21)
 time_to_run <- c()
 for (i in 1:3){
   t_start <- Sys.time()
-  test_integrators(10, ddep = ddep_vec[i], K = 40)
+  test_integrators(1, ddep = ddep_vec[i], K = 40)
   t_end <- Sys.time()
   time_to_run[i] <- t_end - t_start
 }
