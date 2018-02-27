d8a1a13419acd9fe720435b407c600c22d326ace
Neves-P <10762799+Neves-P@users.noreply.github.com>
2018-02-27 10:08:06 +0100

    Fixed wrong parameters in runcode

--- run code.R
+++ run code.R
@@ -3,12 +3,12 @@
 source("make_rhs_string.R")
 
 
-ddep_vec <- c(0, 1, 12)
+ddep_vec <- c(21)
 
 time_to_run <- c()
-for (i in 3){
+for (i in 1:2){
   t_start <- Sys.time()
-  test_integrators(300, ddep = ddep_vec[i], kk = 1, K = 40)
+  test_integrators(10, ddep = ddep_vec[i], kk = 1, K = 40)
   t_end <- Sys.time()
   time_to_run[i] <- t_end - t_start
 }
